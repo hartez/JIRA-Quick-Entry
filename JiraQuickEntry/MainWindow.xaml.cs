3886c9f00211056bb604ad1ba195b9296bcf39e1
E.Z. Hart <hartez@gmail.com>
2011-10-29 14:50:08 -0600

    Added project and issue type selection

--- JiraQuickEntry/MainWindow.xaml.cs
+++ JiraQuickEntry/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
@@ -10,34 +11,44 @@ namespace JiraQuickEntry
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private string _token;
 		private JiraSoapServiceClient _client;
+		private List<RemoteIssueType> _issueTypes = new List<RemoteIssueType>();
+		private List<RemoteProject> _projects = new List<RemoteProject>();
+		private string _token;
 		private string _username;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			Loaded += new RoutedEventHandler(MainWindow_Loaded);
-			KeyDown += new System.Windows.Input.KeyEventHandler(MainWindow_KeyDown);
+			Loaded += MainWindow_Loaded;
+			KeyDown += MainWindow_KeyDown;
 		}
 
-		void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		private void MainWindow_KeyDown(object sender, KeyEventArgs e)
 		{
-			if(e.Key == Key.Enter && Keyboard.Modifiers == ModifierKeys.Control)
+			if (e.Key == Key.Enter && Keyboard.Modifiers == ModifierKeys.Control)
 			{
 				SubmitIssue();
 			}
 		}
 
-		void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
 		{
 			var login = new Login();
 			login.ShowDialog();
 
 			_token = login.Token;
 			_username = login.User;
-			_client =  new JiraSoapServiceClient("jirasoapservice-v2");
+			_client = new JiraSoapServiceClient("jirasoapservice-v2");
+
+			_projects = _client.getProjectsNoSchemes(_token).ToList();
+
+			Project.ItemsSource = _projects;
+			Project.SelectedIndex = 0;
+
+			IssueType.ItemsSource = _issueTypes;
+			UpdateIssueTypes();
 		}
 
 		private void SubmitIssue()
@@ -46,18 +57,18 @@ namespace JiraQuickEntry
 			{
 				try
 				{
-					RemoteIssue issue = new RemoteIssue();
-					issue.project = Project.Text;
+					var issue = new RemoteIssue();
+					issue.project = ((RemoteProject)Project.SelectedItem).key;
 					issue.summary = Issue.Text;
 					issue.description = Description.Text;
 
 					// Defaulting to New Feature for the moment
-					issue.type = "2";
+					issue.type = IssueType.SelectedValue.ToString();
 
 					// Assumes assigning to yourself
 					issue.assignee = _username;
 
-					var result = _client.createIssue(_token, issue);
+					RemoteIssue result = _client.createIssue(_token, issue);
 
 					IssuesCreated.Items.Add(result.summary);
 
@@ -80,6 +91,27 @@ namespace JiraQuickEntry
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			SubmitIssue();
+		}
+
+		private void UpdateIssueTypes()
+		{
+			try
+			{
+				RemoteProject project = _client.getProjectById(_token, long.Parse(Project.SelectedValue.ToString()));
+
+				_issueTypes = _client.getIssueTypesForProject(_token, project.id).ToList();
+				IssueType.Items.Refresh();
+				IssueType.SelectedIndex = 0;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void Project_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			UpdateIssueTypes();
 		}
 	}
 }
